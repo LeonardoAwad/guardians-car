f157c650071523b3d2a2733d9b1377eda5cfa427
LeonardoAwad <leonardo.ciriaco10@hotmail.com>
2022-12-21 17:16:29 -0300

    car list and card finished

--- guardian-car-angular/src/app/components/car-list/car-card/car-card.component.ts
+++ guardian-car-angular/src/app/components/car-list/car-card/car-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { CarModel } from '../model/car.model';
 
 @Component({
   selector: 'app-car-card',
   templateUrl: './car-card.component.html',
-  styleUrls: ['./car-card.component.scss']
+  styleUrls: ['./car-card.component.scss'],
 })
-export class CarCardComponent implements OnInit {
+export class CarCardComponent {
+  @Input() car!: CarModel;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  srcReplace(): string {
+    const img = this.car.name == 'Ford Ká' ? 'Ka' : this.car.name;
+    return `../../../../assets/Carros/${img}.png`;
   }
-
 }
--- guardian-car-angular/src/app/components/car-list/car-list.component.ts
+++ guardian-car-angular/src/app/components/car-list/car-list.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import * as carrosJson from '../../../assets/carros.json';
+
+import { Component, Input, OnInit } from '@angular/core';
+
+import { CarModel } from './model/car.model';
+import { FilterService } from 'src/app/services/filter.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.component.html',
-  styleUrls: ['./car-list.component.scss']
+  styleUrls: ['./car-list.component.scss'],
 })
 export class CarListComponent implements OnInit {
-
-  constructor() { }
+  @Input() carList!: CarModel[];
+  constructor(private filterService: FilterService) {}
 
   ngOnInit(): void {
+    this.filterInit;
   }
 
+  filterInit(): void {
+    const cars: CarModel[] = Object.values(carrosJson);
+    this.filterService.filter$
+      .pipe(
+        tap({
+          next: (filter) => {
+            let carFilted: CarModel[] = cars.filter((car) => {
+              return car.name
+                ?.toLowerCase()
+                .includes(filter.search?.toLowerCase() ?? '');
+            });
+
+            filter.coachwork.forEach((fil) => {
+              if (!fil.selected) {
+                return;
+              }
+              const carType = cars.filter((car) => {
+                return car.type?.includes(fil.value ?? '');
+              });
+
+              carFilted = [...carType, ...carFilted];
+            });
+
+            filter.engines.forEach((fil) => {
+              if (!fil.selected) {
+                return;
+              }
+
+              const carEngine = carFilted.filter((car) => {
+                return car.engine?.includes(fil.value ?? '');
+              });
+              carFilted = carEngine;
+            });
+
+            filter.sizes.forEach((fil) => {
+              if (!fil.selected) {
+                return;
+              }
+
+              const carEngine = carFilted.filter((car) => {
+                return car.size?.includes(fil.value ?? '');
+              });
+
+              carFilted = carEngine;
+            });
+
+            this.carList = carFilted;
+          },
+        })
+      )
+      .subscribe();
+  }
 }
