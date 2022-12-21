a7353226cd1c2f384b1be0a2d3948205ce707363
LeonardoAwad <leonardo.ciriaco10@hotmail.com>
2022-12-21 19:11:03 -0300

    fix bugs

--- guardian-car-angular/src/app/components/car-list/car-list.component.ts
+++ guardian-car-angular/src/app/components/car-list/car-list.component.ts
@@ -3,6 +3,7 @@ import * as carrosJson from '../../../assets/carros.json';
 import { Component, Input, OnInit } from '@angular/core';
 
 import { CarModel } from './model/car.model';
+import { FilterModel } from '../filter/model/filter.model';
 import { FilterService } from 'src/app/services/filter.service';
 import { tap } from 'rxjs/operators';
 
@@ -16,59 +17,72 @@ export class CarListComponent implements OnInit {
   constructor(private filterService: FilterService) {}
 
   ngOnInit(): void {
-    this.filterInit;
+    this.filterInit();
   }
 
   filterInit(): void {
-    const cars: CarModel[] = Object.values(carrosJson);
     this.filterService.filter$
       .pipe(
         tap({
           next: (filter) => {
-            let carFilted: CarModel[] = cars.filter((car) => {
-              return car.name
-                ?.toLowerCase()
-                .includes(filter.search?.toLowerCase() ?? '');
-            });
+            const filtersActive: FilterModel = {
+              coachwork: filter.coachwork.filter((x) => x.selected == true),
+              engines: filter.engines.filter((x) => x.selected == true),
+              sizes: filter.sizes.filter((x) => x.selected == true),
+              search: filter.search,
+            };
 
-            filter.coachwork.forEach((fil) => {
-              if (!fil.selected) {
-                return;
-              }
-              const carType = cars.filter((car) => {
-                return car.type?.includes(fil.value ?? '');
-              });
-
-              carFilted = [...carType, ...carFilted];
-            });
-
-            filter.engines.forEach((fil) => {
-              if (!fil.selected) {
-                return;
-              }
-
-              const carEngine = carFilted.filter((car) => {
-                return car.engine?.includes(fil.value ?? '');
-              });
-              carFilted = carEngine;
-            });
-
-            filter.sizes.forEach((fil) => {
-              if (!fil.selected) {
-                return;
-              }
-
-              const carEngine = carFilted.filter((car) => {
-                return car.size?.includes(fil.value ?? '');
-              });
-
-              carFilted = carEngine;
-            });
-
-            this.carList = carFilted;
+            this.filterCars(filtersActive);
           },
         })
       )
       .subscribe();
+  }
+
+  removeDuplicate(carList: CarModel[]): CarModel[] {
+    carList = carList.filter(
+      (value, index, self) =>
+        index === self.findIndex((car) => car.name === value.name)
+    );
+
+    return carList;
+  }
+
+  filterCars(filtersActive: FilterModel): void {
+    const cars: CarModel[] = Object.values(carrosJson);
+    let carFilted: CarModel[] = cars.filter((car) => {
+      return car.name?.includes(filtersActive.search ?? '');
+    });
+
+    let carType: CarModel[] = [];
+    filtersActive.coachwork.forEach((fil) => {
+      const type = carFilted.filter((car) => car.type == fil.value);
+      carType = [...carType, ...type];
+    });
+
+    if (carType.length || filtersActive.coachwork.length) {
+      carFilted = this.removeDuplicate(carType);
+    }
+
+    let carEngine: CarModel[] = [];
+    filtersActive.engines.forEach((fil) => {
+      const engine = carFilted.filter((car) => car.engine == fil.value);
+
+      carEngine = [...carEngine, ...engine];
+    });
+    if (carEngine.length || filtersActive.engines.length) {
+      carFilted = this.removeDuplicate(carEngine);
+    }
+
+    let carSize: CarModel[] = [];
+    filtersActive.sizes.forEach((fil) => {
+      const size = carFilted.filter((car) => car.size == fil.value);
+      carSize = [...carSize, ...size];
+    });
+    if (carSize.length || filtersActive.sizes.length) {
+      carFilted = this.removeDuplicate(carSize);
+    }
+
+    this.carList = carFilted;
   }
 }
